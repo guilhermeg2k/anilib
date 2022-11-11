afb03607674c0f83a78a29fe692da242306eaa83
guilhermeg2k <guilhermen2k@gmail.com>
2022-11-10 21:16:43 -0300

    Improve special characters regex

--- src/backend/constants/regexConstants.ts
+++ src/backend/constants/regexConstants.ts
@@ -1,4 +1,4 @@
-export const SQUARE_BRACKET_CONTENT_REGEX = /(\[[^\].*]*\])/gi;
-export const PARENTHESES_CONTENT_REGEX = /(\([^).*]*\))/gi;
-export const BRACES_CONTENT_REGEX = /(\{[^}.*]*\})/gi;
-export const NOT_ALPHANUMERIC_REGEX = /[^a-zA-Z0-9]/gi;
+export const SQUARE_BRACKET_CONTENT_REGEX = /(\[[^\]]*\])/gi;
+export const PARENTHESES_CONTENT_REGEX = /(\([^)]*\))/gi;
+export const BRACES_CONTENT_REGEX = /(\{[^}]*\})/gi;
+export const NOT_ALPHANUMERIC_REGEX = /[^A-Za-zÀ-ÿ0-9]/gi;
