105d223635ccf0baf9a0ff035013586473d93620
guilhermeg2k <guilhermen2k@gmail.com>
2023-04-16 18:22:07 -0300

    Update anime repository to use prisma

--- src/backend/repository/anime.ts
+++ src/backend/repository/anime.ts
@@ -1,55 +1,130 @@
-import database from 'backend/database';
-import { Anime } from '@common/types/database';
+import { prisma } from '@backend/database/prisma';
+import {
+  AnimeCreateInput,
+  AnimeTitleCreateInput,
+  GenreCreateInput,
+  SeasonCreteInput,
+  StudioCreateInput,
+} from '@common/types/database';
 import { isPathRelativeToDir } from '@common/utils/file';
-import { v4 as uuid } from 'uuid';
+import { Anime } from '@prisma/client';
 import EpisodeRepository from './episode';
 
 class AnimeRepository {
-  static list() {
-    const animesList = new Array<Anime>();
-    const animes = <Map<string, Anime>>database.list('animes');
-    animes.forEach((anime, id) => {
-      animesList.push({
-        ...anime,
-        id,
-      });
+  static async list() {
+    return await prisma.anime.findMany({
+      include: {
+        season: true,
+        genres: true,
+        studios: true,
+        titles: true,
+      },
     });
-    return animesList;
   }
 
   static getById(id: string) {
-    const anime = <Anime>database.get('animes', id);
-    if (anime) {
-      anime.id = id;
-    }
-    return anime;
+    return prisma.anime.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        season: true,
+        genres: true,
+        studios: true,
+        titles: true,
+      },
+    });
   }
 
-  static listByPath(path: string) {
-    const anime = this.list().find((anime) => anime.folderPath === path);
-    return anime;
+  static async listByPath(path: string) {
+    return await prisma.anime.findFirst({
+      where: {
+        folderPath: path,
+      },
+    });
   }
 
-  static create(anime: Anime) {
-    const key = uuid();
-    const createdAnime = database.insertOrUpdate('animes', key, anime);
-    return <Anime>createdAnime;
+  static async create(
+    anime: AnimeCreateInput,
+    titles: AnimeTitleCreateInput[],
+    studios: StudioCreateInput[],
+    genres: GenreCreateInput[],
+    season: SeasonCreteInput
+  ) {
+    return await prisma.anime.create({
+      data: {
+        ...anime,
+        titles: {
+          create: titles,
+        },
+        season: {
+          connectOrCreate: {
+            where: {
+              name_year: season,
+            },
+            create: season,
+          },
+        },
+        genres: {
+          connectOrCreate: genres.map((genre) => ({
+            where: {
+              name: genre.name,
+            },
+            create: genre,
+          })),
+        },
+        studios: {
+          connectOrCreate: studios.map((studio) => ({
+            where: {
+              anilistID: studio.anilistID,
+            },
+            create: studio,
+          })),
+        },
+      },
+    });
   }
 
-  static update(anime: Anime) {
-    database.insertOrUpdate('animes', anime.id!, anime);
+  static async update(anime: Anime) {
+    return await prisma.anime.update({
+      where: {
+        id: anime.id,
+      },
+      data: anime,
+    });
   }
 
-  static deleteById(id: string) {
-    database.delete('animes', id);
-    EpisodeRepository.deleteByAnimeId(id);
+  static async deleteById(id: string) {
+    await EpisodeRepository.deleteByAnimeId(id);
+    return await prisma.anime.delete({
+      where: {
+        id,
+      },
+    });
   }
 
-  static deleteByDirectory(directory: string) {
-    const animesToDelete = this.list().filter((anime) =>
+  static async deleteByIDS(ids: string[]) {
+    return await prisma.anime.deleteMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+  }
+
+  static async deleteByDirectory(directory: string) {
+    const animesToDelete = (await this.list()).filter((anime) =>
       isPathRelativeToDir(directory, anime.folderPath)
     );
-    animesToDelete.forEach((anime) => this.deleteById(anime.id!));
+
+    return await prisma.anime.deleteMany({
+      where: {
+        id: {
+          in: animesToDelete.map((anime) => anime.id),
+        },
+      },
+    });
   }
 }
 
